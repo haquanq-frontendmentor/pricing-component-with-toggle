9ef17204fdee98d6ef80ca3507e686f0f79c6690
Ha Quanq <quangha0120010@gmail.com>
2025-07-29 11:23:29 +0700

    Refactor switch styles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const monthlyPriceElements = document.querySelectorAll(".plan__price--monthly");
 const annuallyPriceElements = document.querySelectorAll(".plan__price--annually");
 
-document.querySelector(".switch__option--input.monthly").addEventListener("change", (e) => {
-  monthlyPriceElements.forEach((el) => (el.style.display = "block"));
-  annuallyPriceElements.forEach((el) => (el.style.display = "none"));
+document.querySelector(".switch__input--monthly").addEventListener("change", (e) => {
+    monthlyPriceElements.forEach((el) => (el.style.display = "block"));
+    annuallyPriceElements.forEach((el) => (el.style.display = "none"));
 });
 
-document.querySelector(".switch__option--input.annually").addEventListener("change", (e) => {
-  annuallyPriceElements.forEach((el) => (el.style.display = "block"));
-  monthlyPriceElements.forEach((el) => (el.style.display = "none"));
+document.querySelector(".switch__input--annually").addEventListener("change", (e) => {
+    annuallyPriceElements.forEach((el) => (el.style.display = "block"));
+    monthlyPriceElements.forEach((el) => (el.style.display = "none"));
 });
